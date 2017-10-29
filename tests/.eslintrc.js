ae031e1b4dff4c68d0fb33e3b43a8cf3967a0944
Rob Welan <robwelan@yahoo.com.au>
2017-10-29 16:14:36 +1000

    Follow tutorial
     - up to https://guides.emberjs.com/v2.16.0/tutorial/service/
     - API CoT

--- tests/.eslintrc.js
+++ tests/.eslintrc.js
@@ -1,5 +1,15 @@
 module.exports = {
+  globals: {
+    server: true
+  },
+  root: true,
+  parserOptions: {
+    ecmaVersion: 2017,
+    sourceType: 'module'
+  },
+  extends: 'eslint:recommended',
   env: {
-    embertest: true
-  }
+    browser: true
+  },
+  rules: {}
 };
